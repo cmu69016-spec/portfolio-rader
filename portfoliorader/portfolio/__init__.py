0f17ffd841004e969d724be81da21d46fe33dfdf
ChenXin Li <cmu69016@gmail.com>
2026-06-07 15:50:36 +0800

    Delete portfoliorader directory

--- portfoliorader/portfolio/__init__.py
+++
@@ -1,6 +0,0 @@
-"""PortfolioRadar repository entrypoint.
-
-Runtime modules are implemented under ``app`` according to the implementation
-plan. This package keeps the Cursor-facing project folder named ``portfolio``.
-"""
-
